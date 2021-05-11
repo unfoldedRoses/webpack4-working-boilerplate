3b57c59fd00e2009d376a6bfeec932efaeb47a69
unfoldedRoses <ptest5557@gmail.com>
2021-05-11 13:29:33 +0530

    second part of loaders sass jsx css style loaders added

--- app/app.js
+++ app/app.js
@@ -1,1 +1,21 @@
-console.log("I Love REACTJS dsdsd ssss ssss ssss sdsds xxxx ") 
+import React, { Component } from 'react'
+import ReactDOM from "react-dom"
+import "./styles/main.scss"
+// console.log("I Love REACTJS dsdsd ssss ssss ssss sdsds xxxx ") 
+
+
+// fetch(`https://jsonplaceholder.typicode.com/posts`)
+//    .then(res => res.json())
+//    .then(posts => console.log(posts))
+
+//    console.log(posts)
+
+function App(){
+ return  <h1>Hellow From Webpack!</h1>
+}
+
+ReactDOM.render(<App/>,document.getElementById("app"))
+
+if(module.hot){
+    module.hot.accept()
+}
--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,26 @@ module.exports={
     },
     // watch:true
    devServer:{port:8080,
-    contentBase:path.resolve(__dirname,"dist")
-
+    contentBase:path.resolve(__dirname,"dist"),
+    hot:true
+    },
+    module:{
+        rules:[
+        {
+            test: /\.scss$/,
+            use:["style-loader","css-loader","sass-loader"]
+        },
+            {
+                test: /\.js$/,
+                exclude:/(node_modules|bower_components)/,
+                use:{
+                    loader:"babel-loader",
+                    options:{
+                        presets:['@babel/preset-env',"@babel/preset-react"]
+                    }
+                }
+            }
+        ]
     }
     
 }
